b1a7fe774f1881b9fc516c795fa320206291a321
matia-lee <matialee9@gmail.com>
2024-03-04 23:00:05 -0500

    Initial commit

---
+++ frontend/src/components/Homepage.jsx
@@ -0,0 +1,11 @@
+import '../static/Homepage.css'
+
+const Homepage = () => {
+    return (
+        <div className="test">
+            Hello World
+        </div>
+    );
+};
+
+export default Homepage;
