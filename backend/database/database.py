0adaf3413e324369e5ad0218ba7fd5e72a7e78e0
matia-lee <matialee9@gmail.com>
2024-03-06 10:31:08 -0500

    initiate db

---
+++ backend/database/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from models import Base
+import os
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+
+DATABASE_URI = f"mysql+pymysql://{os.environ.get('db_user')}:{os.environ.get('db_password')}@localhost/outfitly"
+
+
+engine = create_engine(DATABASE_URI)
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
