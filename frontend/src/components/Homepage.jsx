3c18313498efb9287f8ffc452d8e4b0a169b450d
matia-lee <matialee9@gmail.com>
2024-03-05 22:03:15 -0500

    homepage

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -2,9 +2,13 @@ import "../static/Homepage.css";
 
 const Homepage = () => {
   return (
-    <div className="test">
-      <div>Hello</div>
-      <div>world</div>
+    <div className="main-container">
+      <h1 className="title">Outfitly</h1>
+      <h3 className="catchphrase">Outfits made simply.</h3>
+      <button className="signup">Signup to continue</button>
+      <h4 className="login">
+        Already have an account? <span className="login-link">Log in</span>
+      </h4>
     </div>
   );
 };
