6f04a487a8613dd189fad2360962094ebbcdcf0b
matia-lee <matialee9@gmail.com>
2024-03-05 22:17:58 -0500

    homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
