b048e189b8b3d3856fbf05f08641b0fdfb5bbc2e
matia-lee <matialee9@gmail.com>
2024-03-06 00:34:33 -0500

    sign up page

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,10 +1,44 @@
 import "../static/Signup.css";
+import Hanger from "../icons/Hanger";
+import ProfileIcon from "../icons/ProfileIcon";
+import EmailIcon from "../icons/EmailIcon";
+import PasswordIcon from "../icons/PasswordIcon";
 
 const Signup = () => {
   return (
     <div className="signup-container">
-      <div>hello</div>
-      <div>world</div>
+      <div className="icon">
+        <Hanger />
+      </div>
+      <div className="title">Outfitly</div>
+      <h6 className="subtitle">Create account</h6>
+      <div className="create-username">
+        <div className="profile-icon">
+          <ProfileIcon />
+        </div>
+        <input
+          className="username-input"
+          type="text"
+          placeholder="Create username"
+        />
+      </div>
+      <div className="enter-email">
+        <div className="email-icon">
+          <EmailIcon />
+        </div>
+        <input className="email-input" type="email" placeholder="Enter email" />
+      </div>
+      <div className="enter-password">
+        <div className="password-icon">
+          <PasswordIcon />
+        </div>
+        <input
+          className="password-input"
+          type="password"
+          placeholder="Enter password"
+        />
+      </div>
+      <button className="signup-button">Sign Up :)</button>
     </div>
   );
 };
