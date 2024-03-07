fdd00789cad69d727b241aa0bd0813e4333a0c1c
matia-lee <matialee9@gmail.com>
2024-03-07 14:33:45 -0500

    upload page

--- frontend/src/components/Upload.jsx
+++ frontend/src/components/Upload.jsx
@@ -1,17 +1,26 @@
 import "../static/Upload.css";
 import Hanger from "../icons/Hanger";
+import UploadIcon from "../icons/UploadIcon";
 
 const Upload = () => {
   return (
-    <div className="navbar">
-      <div className="uploadpage-icon">
-        <Hanger color="#ff9999" />
+    <>
+      <div className="navbar">
+        <div className="uploadpage-icon">
+          <Hanger color="#ff9999" />
+        </div>
+        <div className="logo">
+          <div className="title">Outfitly</div>
+          <div className="subtitle">Upload images</div>
+        </div>
       </div>
-      <div className="logo">
-        <div className="title">Outfitly</div>
-        <div className="subtitle">Upload images</div>
+      <div className="upload-container">
+        <UploadIcon size="80px" color="#ff9999" />
+        <div className="instructions">
+          <h3>Drag files to upload</h3>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
