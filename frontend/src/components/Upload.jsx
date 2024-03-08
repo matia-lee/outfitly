f38e51bb323163eac5e0a82b48463163f060dcab
matia-lee <matialee9@gmail.com>
2024-03-07 19:46:00 -0500

    boto3

--- frontend/src/components/Upload.jsx
+++ frontend/src/components/Upload.jsx
@@ -1,8 +1,58 @@
 import "../static/Upload.css";
+import { useCallback, useState, useRef } from "react";
 import Hanger from "../icons/Hanger";
 import UploadIcon from "../icons/UploadIcon";
 
 const Upload = () => {
+  const [dragOver, setDragOver] = useState(false);
+  const fileInputRef = useRef(null);
+
+  const handleBrowseFileClick = () => {
+    setTimeout(() => {
+      fileInputRef.current.click();
+    }, 20);
+  };
+
+  const handleDragOver = useCallback((e) => {
+    e.preventDefault();
+    setDragOver(true);
+  }, []);
+
+  const handleDragLeave = useCallback((e) => {
+    e.preventDefault();
+    setDragOver(false);
+  }, []);
+
+  const handleDrop = useCallback((e) => {
+    e.preventDefault();
+    setDragOver(false);
+    const files = e.dataTransfer.files;
+    if (files.length) {
+      handleFileUpload(files[0]);
+    }
+  }, []);
+
+  const handleFileUpload = (file) => {
+    // console.log(file);
+    const formData = new FormData();
+    formData.append("file", file);
+
+    fetch("/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("File uploaded successfully:", data);
+      })
+      .catch((error) => console.error("Error uploading file:", error));
+  };
+
   return (
     <>
       <div className="navbar">
@@ -14,10 +64,23 @@ const Upload = () => {
           <div className="subtitle">Upload images</div>
         </div>
       </div>
-      <div className="upload-container">
+      <div
+        className={`upload-container ${dragOver ? "drag-over" : ""}`}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+      >
         <UploadIcon size="80px" color="#ff9999" />
         <div className="instructions">
           <h3>Drag files to upload</h3>
+          <p>or</p>
+          <input type="file" style={{ display: "none" }} ref={fileInputRef} />
+          <button
+            className="browse-files-button"
+            onClick={handleBrowseFileClick}
+          >
+            Browse files
+          </button>
         </div>
       </div>
     </>
