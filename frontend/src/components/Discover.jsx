e85ec65b5709af6bad3975c83de8f7b72dfad881
matia-lee <matialee9@gmail.com>
2024-03-18 12:20:13 -0400

    styling

--- frontend/src/components/Discover.jsx
+++ frontend/src/components/Discover.jsx
@@ -1,9 +1,35 @@
 import "../static/Discover.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Hanger from "../icons/Hanger";
 
 const Discover = () => {
+  const [spin, setSpin] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleHangerClick = () => {
+    setSpin(true);
+    setTimeout(() => setSpin(false), 600);
+  };
+
+  const handleLogoClick = () => {
+    navigate("/homepage");
+  };
+
   return (
     <div>
-      <div>hello world</div>
+      <div className="navbar">
+        <div className="uploadpage-icon" onClick={handleHangerClick}>
+          <Hanger className={spin ? "spin-animation" : ""} color="#ff9999" />
+        </div>
+        <div className="logo">
+          <div className="title" onClick={handleLogoClick}>
+            Outfitly
+          </div>
+          <div className="closet-subtitle">Discover</div>
+        </div>
+      </div>
     </div>
   );
 };
