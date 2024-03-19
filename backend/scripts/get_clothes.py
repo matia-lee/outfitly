e6aee37d6626a5efe33ecb3f8b6e28fdb5e42997
matia-lee <matialee9@gmail.com>
2024-03-19 15:59:55 -0400

    sort by newest added first

--- backend/scripts/get_clothes.py
+++ backend/scripts/get_clothes.py
@@ -1,12 +1,13 @@
 from flask import request, jsonify
 from database.database import db_session
 from database.models import ImageModel
+from sqlalchemy import desc
 
 def get_clothes():
     interaction_filter = request.args.get('interaction')
     if interaction_filter:
-        clothes = db_session.query(ImageModel).filter(ImageModel.interaction == interaction_filter)
+        clothes = db_session.query(ImageModel).filter(ImageModel.interaction == interaction_filter).order_by(desc(ImageModel.id))
     else:
-        clothes = db_session.query(ImageModel)
+        clothes = db_session.query(ImageModel).order_by(desc(ImageModel.id))
     clothes_list = [{"id": image.id, "username": image.username, "file_url": image.file_url, "interaction": image.interaction, "like": image.like} for image in clothes]
     return jsonify(clothes_list)
--- backend/scripts/get_fits.py
+++ backend/scripts/get_fits.py
@@ -1,8 +1,9 @@
 from flask import jsonify
 from database.database import db_session
 from database.models import Outfits
+from sqlalchemy import desc
 
 def get_fits():
-    clothes = db_session.query(Outfits)
+    clothes = db_session.query(Outfits).order_by(desc(Outfits.outfit_id))
     clothes_list = [{"outfit_id": image.outfit_id, "username": image.username, "headwear": image.headwear, "top": image.top, "bottom": image.bottom, "footwear": image.footwear, "fit_name": image.fit_name} for image in clothes]
     return jsonify(clothes_list)
